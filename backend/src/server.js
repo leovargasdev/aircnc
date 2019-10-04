2e219095aba9c1860afbad2307d880d237336ad2
Leo Vargas <leu1607@hotmail.com>
2019-10-04 14:18:48 -0300

    adicionando arquivos iniciais do frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const app = express();
@@ -9,7 +11,8 @@ mongoose.connect('mongodb+srv://vargas:uva007@minhabase-zmb6f.mongodb.net/rocket
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000);
+app.listen(3333);
